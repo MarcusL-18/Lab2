562549ff691d0004a1918cc141cb37d6864b8b56
Marcus <MARCUSLYH18.25@ichat.sp.edu.sg>
2025-05-08 16:21:09 +0800

    Add BMI

---
+++ bmi.py
@@ -0,0 +1,10 @@
+def calc_bmi(height, weight):
+    BMI = weight/(height*height)
+    
+    print("Height=" + str(height))
+    print("Weight=" + str(weight))
+    print("BMI: " , BMI)
+
+
+
+calc_bmi(weight= 57 , height= 1.73)
